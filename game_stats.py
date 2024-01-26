62d3fd226693b55c9bb84d34545f8d9169d019ff
berrgrayson <berrgrayson@yahoo.com>
2024-01-27 00:27:08 +0200

    High Scores.

--- game_stats.py
+++ game_stats.py
@@ -5,6 +5,8 @@ class GameStats:
         """Initialize statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
+        # High score should never be reset.
+        self.high_score = 0
     
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
